cdd1ff198f0383aeb18dd3afd5c3b4006f7e8d42
namgyunKim <skarbs01@naver.com>
2024-05-03 16:55:23 +0900

    fix : 캡슐화 제거 (프라이빗에 접근이 안댐...)

--- src/domain/member/inpus/create-member.request.ts
+++ src/domain/member/inpus/create-member.request.ts
@@ -4,28 +4,12 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateMemberRequest {
   @ApiProperty({ example: '홍길동', description: '이름' })
   @IsString()
-  private readonly _name: string;
+  readonly name: string;
   @ApiProperty({ example: 'skarbs1993@naver.com', description: '이메일' })
   @IsEmail()
-  private readonly _email: string;
+  readonly email: string;
   @ApiProperty({ example: '123456', description: '비밀번호' })
   @IsString()
   @MinLength(6)
-  private _password: string;
-
-  get password(): string {
-    return this._password;
-  }
-
-  set password(password: string) {
-    this._password = password;
-  }
-
-  get email(): string {
-    return this._email;
-  }
-
-  get name(): string {
-    return this._name;
-  }
+  password: string;
 }
