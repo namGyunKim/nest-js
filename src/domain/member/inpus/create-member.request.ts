15587b1b831166581d965d52260da868d0de1934
namgyunKim <skarbs01@naver.com>
2024-05-03 15:26:12 +0900

    feat : dto 캡슐화 적용

--- src/domain/member/inpus/create-member.request.ts
+++ src/domain/member/inpus/create-member.request.ts
@@ -1,16 +1,31 @@
-// create-user.dto.ts
 import { IsEmail, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateMemberRequest {
   @ApiProperty({ example: '홍길동', description: '이름' })
   @IsString()
-  readonly name: string;
+  private readonly _name: string;
   @ApiProperty({ example: 'skarbs1993@naver.com', description: '이메일' })
   @IsEmail()
-  readonly email: string;
+  private readonly _email: string;
   @ApiProperty({ example: '123456', description: '비밀번호' })
   @IsString()
   @MinLength(6)
-  readonly password: string;
+  private _password: string;
+
+  get password(): string {
+    return this._password;
+  }
+
+  set password(password: string) {
+    this._password = password;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get name(): string {
+    return this._name;
+  }
 }
--- src/domain/member/operations/mutation/member-mutation.service.ts
+++ src/domain/member/operations/mutation/member-mutation.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { MemberEntity } from '../../models/member.entity';
 import { CreateMemberRequest } from '../../inpus/create-member.request';
 import { CreateGlobalResponse } from '../../../../global/dto/create-global.response';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class MemberMutationService {
@@ -15,6 +16,8 @@ export class MemberMutationService {
   async createMember(
     request: CreateMemberRequest,
   ): Promise<CreateGlobalResponse> {
+    const saltOrRounds = 10;
+    request.password = await bcrypt.hash(request.password, saltOrRounds);
     const newMember = this.memberRepository.create(request);
     const memberEntity = await this.memberRepository.save(newMember);
     return new CreateGlobalResponse(memberEntity.id);
