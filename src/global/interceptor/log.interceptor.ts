a1ceaf2c6216c854e1c3a9afcbad0a73af1cceb1
namgyunKim <skarbs01@naver.com>
2024-05-03 17:34:56 +0900

    fix : 에러가 났을경우에도 로그가 남게 인터셉터 수정

--- src/global/interceptor/log.interceptor.ts
+++ src/global/interceptor/log.interceptor.ts
@@ -1,13 +1,7 @@
-import {
-  Injectable,
-  NestInterceptor,
-  ExecutionContext,
-  CallHandler,
-  Logger,
-} from '@nestjs/common';
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { Request } from 'express'; // Express 타입 사용
+import { Request } from 'express';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
@@ -17,17 +11,21 @@ export class LoggingInterceptor implements NestInterceptor {
     const now = Date.now();
     const method = context.getHandler().name;
     const className = context.getClass().name;
-    const req = context.switchToHttp().getRequest<Request>(); // HTTP 요청 객체 가져오기
-    const { method: httpMethod, url, body, params, query } = req; // 요청에서 필요한 정보 추출
+    const req = context.switchToHttp().getRequest<Request>();
+    const { method: httpMethod, url, body, params, query } = req;
+
+    // 로그 기록 (요청 시작)
+    this.logger.log(
+      `${className}.${method}() - Start` +
+        `\nHTTP Method: ${httpMethod} URL: ${url}` +
+        `\nParams: ${JSON.stringify(params)} Query: ${JSON.stringify(query)} Body: ${JSON.stringify(body)}`,
+    );
 
     return next.handle().pipe(
       tap(() => {
         const responseTime = Date.now() - now;
-        this.logger.log(
-          `${className}.${method}() - ${responseTime}ms` +
-            `\nHTTP Method: ${httpMethod} URL: ${url}` +
-            `\nParams: ${JSON.stringify(params)} Query: ${JSON.stringify(query)} Body: ${JSON.stringify(body)}`,
-        );
+        // 로그 기록 (응답 완료)
+        this.logger.log(`${className}.${method}() - End - ${responseTime}ms`);
       }),
     );
   }
